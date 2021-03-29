a49d1bb0c4624a91f398610d04fc32d4c539d43f
NeelBains <50185230+NeelBains@users.noreply.github.com>
2021-03-28 21:25:08 -0400

    sure

---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockController : MonoBehaviour
+{
+
+    [SerializeField]
+    Text codeText;
+    string codeTextValue = "";
+    [SerializeField]
+    GameObject Door1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        codeText.text = codeTextValue;
+
+        if (codeTextValue == "1234")
+        {
+            Door1.GetComponent<Animator>().SetBool("Open",true);
+
+        }
+
+        if (codeTextValue.Length >= 4)
+            codeTextValue = "";
+    }
+
+    public void AddDigit(string digit)
+    {
+        codeTextValue += digit;
+    }
+
+}
