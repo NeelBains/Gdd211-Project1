0f42415366137cef0dde0193ff7919e93e313eb6
NeelBains <50185230+NeelBains@users.noreply.github.com>
2021-04-05 14:58:43 -0400

    Final

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void ButtonPressed()
+    {
+        SceneManager.LoadScene("Escape");
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+            SceneManager.LoadScene("end");
+    }
+}
